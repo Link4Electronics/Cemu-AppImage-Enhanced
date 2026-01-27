b2c720844ba7ba4a056c116cde10a498c432f46a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-27 01:41:17 -0400

    `make-aur-package` now targets v3

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,9 +16,4 @@ echo "Building cemu..."
 echo "---------------------------------------------------------------"
 
 # build with x86_64_v3 target
-if [ "$ARCH" = 'x86_64' ]; then
-	sed -i -e 's|march=x86-64|march=x86-64-v3|g' /etc/makepkg.conf
-	cat /etc/makepkg.conf
-fi
-
-make-aur-package cemu
+TARGET_V3_CPU=1 make-aur-package cemu
