54ba3cb7b9bec36ef0b4c7479dc93c5a4f80dda0
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-27 00:44:17 -0400

    add `x86-64-v3` hook

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q cemu | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.bg.hook:x86-64-v3-check.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/cemu.png
 export DESKTOP=/usr/share/applications/cemu.desktop
