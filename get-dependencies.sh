0d3a775692c1577e0d76039eb7cc13119466d605
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-27 00:27:30 -0400

    update to template and target x86-64-v3

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,48 +1,24 @@
 #!/bin/sh
 
-set -eux
-ARCH="$(uname -m)"
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+set -eu
 
-echo "Installing dependencies..."
+ARCH=$(uname -m)
+
+echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-	base-devel        \
-	curl              \
-	git               \
-	libx11            \
-	libxrandr         \
-	libxss            \
-	pipewire-audio    \
-	pulseaudio        \
-	pulseaudio-alsa   \
-	wget              \
-	xorg-server-xvfb  \
-	zsync
+pacman -Syu --noconfirm pipewire-audio pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common
-
+get-debloated-pkgs.sh --add-common
 
 echo "Building cemu..."
 echo "---------------------------------------------------------------"
-sed -i -e 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
-sed -i \
-	-e 's|-O2|-O3|'                              \
-	-e 's|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|'  \
-	-e 's|#MAKEFLAGS|MAKEFLAGS|'                 \
-	/etc/makepkg.conf
-cat /etc/makepkg.conf
 
-git clone --depth 1 https://aur.archlinux.org/cemu.git ./cemu && (
-	cd ./cemu
-	sed -i -e "s|x86_64|$ARCH|" ./PKGBUILD
-	makepkg -fs --noconfirm
-	ls -la .
-	pacman --noconfirm -U ./*.pkg.tar.*
-)
+# build with x86_64_v3 target
+if [ "$ARCH" = 'x86_64' ]; then
+	sed -i -e 's|march=x86-64|march=x86-64-v3|g' /etc/makepkg.conf
+	cat /etc/makepkg.conf
+fi
 
-pacman -Q cemu | awk '{print $2; exit}' > ~/version
+make-aur-package --archlinux-pkg cemu
