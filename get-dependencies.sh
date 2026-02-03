e2f853dac053aa66eda16a117face5f1984cc783
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-03 04:39:24 -0400

    fix build

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -48,24 +48,27 @@ fi
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
 	echo "Making nightly build of Cemu..."
     git clone --recursive --depth 1 "$REPO" ./Cemu
-	mkdir -p ./Cemu/build
-	cd ./Cemu/build
+	mkdir -p ./Cemu
+	cd ./Cemu
 	git rev-parse --short=9 HEAD > ~/version
 
 	# CMAKE_POLICY_VERSION_MINIMUM=3.5 required for stable
-	cmake ../ \
-		-D ALLOW_PORTABLE=OFF                    \
-		-D CMAKE_BUILD_TYPE=Release              \
-		-D CMAKE_C_COMPILER=clang                \
-		-D CMAKE_CXX_COMPILER=clang++            \
-		-D CMAKE_EXE_LINKER_FLAGS="-lzstd"       \
-		-D CMAKE_C_FLAGS_RELEASE="$@"            \
-		-D CMAKE_CXX_FLAGS_RELEASE="$@"          \
-		-D CMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
-		-D CMAKE_POLICY_VERSION_MINIMUM=3.5      \
-		-D ENABLE_VCPKG=OFF                      \
-		-Wno-dev
-	make -j $(nproc)
+	mkdir -p ./build && (
+		cd ./build
+		cmake ../ \
+			-D ALLOW_PORTABLE=OFF                    \
+			-D CMAKE_BUILD_TYPE=Release              \
+			-D CMAKE_C_COMPILER=clang                \
+			-D CMAKE_CXX_COMPILER=clang++            \
+			-D CMAKE_EXE_LINKER_FLAGS="-lzstd"       \
+			-D CMAKE_C_FLAGS_RELEASE="$@"            \
+			-D CMAKE_CXX_FLAGS_RELEASE="$@"          \
+			-D CMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
+			-D CMAKE_POLICY_VERSION_MINIMUM=3.5      \
+			-D ENABLE_VCPKG=OFF                      \
+			-Wno-dev
+		make -j $(nproc)
+	)
 
 	mv -v ./bin/Cemu_release /usr/bin/cemu
 	mkdir -p /usr/share/Cemu
