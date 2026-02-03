f49f248b958c0617e3321492c4531e0f5e331549
Link4Electronics <link4electronics@gmail.com>
2026-02-03 03:37:53 -0300

    manually build `aarch64` for nightly (#11)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,20 +6,87 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm pipewire-audio pipewire-jack
+pacman -Syu --noconfirm \
+	bluez-libs          \
+	boost               \
+	clang               \
+	cmake               \
+	fmt                 \
+	glm                 \
+	glslang             \
+	glu                 \
+	hicolor-icon-theme  \
+	hidapi              \
+	libgl               \
+	libzip              \
+	llvm                \
+	nasm                \
+	pipewire-audio      \
+	pipewire-jack       \
+	pugixml             \
+	rapidjson           \
+	sdl2				\
+	vulkan-headers      \
+	wayland-protocols   \
+	zarchive
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common
+get-debloated-pkgs --add-common ! llvm
 
-echo "Building cemu..."
+make-aur-package cubeb
+make-aur-package wxgtk-git
+
+echo "Building Cemu..."
 echo "---------------------------------------------------------------"
-
-# build with x86_64_v3 target
+REPO="https://github.com/cemu-project/Cemu"
+# Determine to build nightly or stable
+#if [ "${DEVEL_RELEASE-}" = 1 ]; then
+#	echo "Making nightly build of Cemu..."
+#	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+#    git clone --recursive --depth 1 "$REPO" ./Cemu
+#else
+#	echo "Making stable build of Cemu..."
+#	VERSION="$(git ls-remote --tags --sort="v:refname" https://github.com/cemu-project/Cemu | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')"
+#	git clone --branch v"$VERSION" --single-branch --recursive --depth 1 "$REPO" ./Cemu
+#fi
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
-	make-aur-package wxgtk-git
-	make-aur-package cubeb
-	TARGET_V3_CPU=1 make-aur-package cemu-git
+	echo "Making nightly build of Cemu..."
+	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+    git clone --recursive --depth 1 "$REPO" ./Cemu
+
+	cd Cemu
+	mkdir -p build && cd build
+
+	ARCH_FLAGS=""
+	if [ "$ARCH" = 'x86_64' ]; then
+		echo "Making x86-64-v3 optimized build of Cemu..."
+		ARCH_FLAGS="-march=x86-64-v3 -O3"
+	fi
+	
+	# CMAKE_POLICY_VERSION_MINIMUM=3.5 required for stable
+	cmake .. -D ALLOW_PORTABLE=OFF \
+		  -D CMAKE_BUILD_TYPE=Release \
+	  	  -D CMAKE_C_COMPILER=clang \
+	 	  -D CMAKE_CXX_COMPILER=clang++ \
+		  -D CMAKE_EXE_LINKER_FLAGS="-lzstd" \
+	 	  -D CMAKE_C_FLAGS_RELEASE="$ARCH_FLAGS -DNDEBUG" \
+		  -D CMAKE_CXX_FLAGS_RELEASE="$ARCH_FLAGS -DNDEBUG" \
+		  -D CMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
+	 	  -D CMAKE_POLICY_VERSION_MINIMUM=3.5 \ 
+		  -D ENABLE_VCPKG=OFF \
+		  -Wno-dev
+	make -j $(nproc)
+
+	install -d /usr/bin /usr/share/Cemu
+	mv bin/Cemu_release /usr/bin/Cemu
+	cp -dr --no-preserve=ownership -t /usr/share/Cemu bin/*
+	install -Dm644 -t /usr/share/applications dist/linux/info.cemu.Cemu.desktop
+	install -Dm644 -t /usr/share/icons/hicolor/128x128/apps dist/linux/info.cemu.Cemu.png
+	install -Dm644 -t /usr/share/metainfo dist/linux/info.cemu.Cemu.metainfo.xml
 else
+	echo "Making stable build of Cemu..."
 	TARGET_V3_CPU=1 make-aur-package cemu
+	VERSION=$(pacman -Q cemu | awk '{print $2; exit}')
 fi
+echo "$VERSION" > ~/version
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,18 +3,29 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q cemu | awk '{print $2; exit}') # example command to get version of application here
+#VERSION=$(pacman -Q cemu | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook:x86-64-v3-check.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/info.cemu.Cemu.png
-export DESKTOP=/usr/share/applications/Cemu.desktop
+
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+  export DESKTOP=/usr/share/applications/info.cemu.Cemu.desktop
+else
+  export DESKTOP=/usr/share/applications/Cemu.desktop
+fi
+
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
+export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/cemu
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+  quick-sharun /usr/bin/Cemu
+else
+  quick-sharun /usr/bin/cemu
+fi
 
 # Additional changes can be done in between here
 
